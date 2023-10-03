a43b74f5f98e7ca5d7623ddb93129a7fcd9c2882
asad612 <muhammad.asad@cinqtech.com>
2023-10-03 12:45:51 +0500

    registration process

--- app/src/main/java/com/example/taxibooking/DoneActivity.java
+++ app/src/main/java/com/example/taxibooking/DoneActivity.java
@@ -11,48 +11,27 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class DoneActivity extends AppCompatActivity {
-    TextView rent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_done);
-        ImageView home = findViewById(R.id.goback);
-        home.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(DoneActivity.this, HomeActivity.class);
-                startActivity(intent);
-            }
+        findViewById(R.id.back).setOnClickListener(v -> {
+            Intent intent = new Intent(DoneActivity.this, HomeActivity.class);
+            startActivity(intent);
         });
+        TextView rent = findViewById(R.id.rent);
         TextView from = findViewById(R.id.from);
         TextView to = findViewById(R.id.to);
-        rent = findViewById(R.id.rent);
         from.setText(HomeActivity.pickup);
         to.setText(DestinationActivity.dropoff);
-
-        RadioButton ac = findViewById(R.id.ac);
-        RadioButton nac = findViewById(R.id.nac);
-        ac.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                rent.setText("Your Total Amount Will Be \n 450 Rs.");
-            }
-        });
-        nac.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                rent.setText("Your Total Amount Will Be \n 300 Rs.");
-            }
-        });
+        findViewById(R.id.ac).setOnClickListener(v -> rent.setText("Your Total Amount Will Be \n 450 Rs."));
+        findViewById(R.id.nac).setOnClickListener(v -> rent.setText("Your Total Amount Will Be \n 300 Rs."));
 
         Button goo = findViewById(R.id.goo);
-        goo.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(DoneActivity.this, "Ride confirmed", Toast.LENGTH_SHORT).show();
-                Intent intent = new Intent(DoneActivity.this, HomeActivity.class);
-                startActivity(intent);
-            }
+        goo.setOnClickListener(v -> {
+            Toast.makeText(DoneActivity.this, "Ride confirmed", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(DoneActivity.this, HomeActivity.class);
+            startActivity(intent);
         });
 
 
