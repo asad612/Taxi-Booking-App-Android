093f280231ec48ff765db48354c2876ed531fe52
asad612 <muhammad.asad@cinqtech.com>
2023-10-03 11:54:34 +0500

    registration process

--- app/src/main/java/com/example/taxibooking/ForgotActivity.java
+++ app/src/main/java/com/example/taxibooking/ForgotActivity.java
@@ -9,28 +9,18 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class ForgotActivity extends AppCompatActivity {
-    Button signIn;
-    TextView sign;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot);
-        signIn = (Button) findViewById(R.id.signIn);
-        signIn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(ForgotActivity.this, HomeActivity.class);
-                Toast.makeText(ForgotActivity.this, "Verification Email sent.", Toast.LENGTH_SHORT).show();
-                startActivity(intent);
-            }
+        findViewById(R.id.btn_forgot).setOnClickListener(v -> {
+            Intent intent = new Intent(ForgotActivity.this, HomeActivity.class);
+            Toast.makeText(ForgotActivity.this, "Verification Email sent.", Toast.LENGTH_SHORT).show();
+            startActivity(intent);
         });
-        sign = (TextView) findViewById(R.id.signintxt);
-        sign.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(ForgotActivity.this, LoginActivity.class);
-                startActivity(intent);
-            }
+        findViewById(R.id.tv_back).setOnClickListener(v -> {
+            Intent intent = new Intent(ForgotActivity.this, LoginActivity.class);
+            startActivity(intent);
         });
     }
 }
--- app/src/main/java/com/example/taxibooking/PanelActivity.java
+++ app/src/main/java/com/example/taxibooking/PanelActivity.java
@@ -7,26 +7,17 @@ import android.view.View;
 import android.widget.Button;
 
 public class PanelActivity extends AppCompatActivity {
-    Button signUp, signIn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_panel);
-        signIn = (Button) findViewById(R.id.signIn);
-        signIn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(PanelActivity.this, LoginActivity.class);
-                startActivity(intent);
-            }
+        findViewById(R.id.btn_signIn).setOnClickListener(v -> {
+            Intent intent = new Intent(PanelActivity.this, LoginActivity.class);
+            startActivity(intent);
         });
-        signUp = (Button) findViewById(R.id.signup);
-        signUp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(PanelActivity.this, SignupActivity.class);
-                startActivity(intent);
-            }
+        findViewById(R.id.btn_signUp).setOnClickListener(v -> {
+            Intent intent = new Intent(PanelActivity.this, SignupActivity.class);
+            startActivity(intent);
         });
     }
 }
